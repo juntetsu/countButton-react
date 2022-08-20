b650cc7aefe294581cd521aba93c29ff06f90a13
Jun <juntetsu.1012@gmail.com>
2022-08-20 20:23:33 +0900

    ナベアツ

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+import aho from "./img/image.png";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+const App = () => {
+  const [num, setNum] = useState(0);
+  const [nabeatsu, setNabeatsu] = useState(false);
+
+  const onClickButton = () => {
+    setNum(num + 1);
+  }
+
+  if (num > 0) {
+    if (num % 3 === 0 || Boolean(num.toString().match(/3/))) {
+        nabeatsu || setNabeatsu(true);
+    } else {
+        nabeatsu && setNabeatsu(false);
+    }
+}
+
+  return(
+    <>
+      <p>{num}</p>
+      <button onClick={onClickButton}>Count Up!</button>
+      {nabeatsu && <div id="image"><img src={aho} /></div>}
+    </>
   );
 }
 
